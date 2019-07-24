ab1337ac6c4ee882f6ff3d4eb6e43cd067285c08
haylie3369 <haylie3369@coupang.com>
2019-07-24 23:33:05 +0900

    [WF-011] 로그 정리

--- src/main/java/consumer/WordConsumer.java
+++ src/main/java/consumer/WordConsumer.java
@@ -84,7 +84,6 @@ public class WordConsumer extends Thread {
                     writerMap.put(fileName, bufferedWriter);
                 }
                 bufferedWriter.append(word);
-                System.out.println(word);
             } catch (IOException e) {
                 throw new WordFunnelException(WordFunnelException.ExceptionType.FILE_IO_EXCEPTION);
             }
